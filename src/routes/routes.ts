ada4b230b9b828386864747fd2903555047c1f91
João Matheus Pereira Andrade <jmatheus.andrade1507@gmail.com>
2026-01-27 15:48:33 -0300

    feat: add cliente registration route and jsonwebtoken dependency

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+import clienteRoutes from './clienteRoutes'
 
 const routes = Router()
 
 routes.get('/teste', (_, res) => {
   res.json({ status: 'ok' })
 })
+
+routes.use('/clientes', clienteRoutes);
 
 export default routes
