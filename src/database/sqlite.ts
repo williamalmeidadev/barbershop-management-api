c9261928b4df3f4f26a11579d8f6f4d6099a5272
Davi Ladeira Balsamao <220748192+davi-balsamao@users.noreply.github.com>
2026-01-28 10:58:31 -0300

    feat: updating sqlite.ts for tests

--- src/database/sqlite.ts
+++ src/database/sqlite.ts
@@ -3,7 +3,11 @@ import path from 'path'
 
 sqlite3.verbose()
 
-const dbPath = path.resolve(__dirname, '../../barbershop.db')
+const isTest = process.env.NODE_ENV === 'test'
+
+const dbPath = isTest 
+  ? ':memory:' 
+  : path.resolve(__dirname, '../../database.sqlite')
 
 export const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
